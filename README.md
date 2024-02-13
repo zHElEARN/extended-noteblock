a638ec4e723490424ba4b6b70008fc7c817ee083
zHElEARN <i@zhelearn.com>
2024-02-14 04:40:46 +0800

    (feat): spawm particle when use noteblock

---
+++ README.md
@@ -0,0 +1,5 @@
+# Extended Noteblock | 更多的音符盒
+
+向Minecraft中添加更多音调的音符盒
+
+一点碎碎念[Notes.md](Notes.md)
--- src/main/java/com/zhelearn/mods/extendednoteblock/blocks/HighNoteBlock.java
+++ src/main/java/com/zhelearn/mods/extendednoteblock/blocks/HighNoteBlock.java
@@ -1,22 +1,16 @@
 package com.zhelearn.mods.extendednoteblock.blocks;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.command.argument.ParticleEffectArgumentType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class HighNoteBlock extends net.minecraft.block.NoteBlock {
@@ -27,15 +21,9 @@ public class HighNoteBlock extends net.minecraft.block.NoteBlock {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
-            int notePitch = state.get(NOTE);
-            float pitch = (float) Math.pow(2.0D, (double) (notePitch - 12) / 12.0D);
-
-            SoundEvent highNoteSoundEvent = Registries.SOUND_EVENT.get(new Identifier("extended-noteblock", "harp_fs6"));
-
-            world.playSound(null, pos, highNoteSoundEvent, SoundCategory.RECORDS, 1.00F, pitch);
-
             state = state.cycle(NOTE);
             world.setBlockState(pos, state, 3);
+            world.addSyncedBlockEvent(pos, this, 0, 0);
         }
 
         return ActionResult.SUCCESS;
@@ -48,7 +36,8 @@ public class HighNoteBlock extends net.minecraft.block.NoteBlock {
 
         SoundEvent highNoteSoundEvent = Registries.SOUND_EVENT.get(new Identifier("extended-noteblock", "harp_fs6"));
 
-        world.playSound(null, pos, highNoteSoundEvent, SoundCategory.RECORDS, 1.00F, pitch);
+        world.playSound(null, pos, highNoteSoundEvent, SoundCategory.RECORDS, 3.00F, pitch);
+        world.addParticle(ParticleTypes.NOTE, (double)pos.getX() + 0.5, (double)pos.getY() + 1.2, (double)pos.getZ() + 0.5, (double)notePitch / 24.0, 0.0, 0.0);
 
         return true;
     }
